74d9c9cc64e53404af5428a4b6a4963f26f7db9c
gwon tae-yun <taeyun1118@naver.com>
2025-05-08 16:24:11 +0900

    fix:enum 수정

--- backend/src/main/java/com/malnutrition/backend/domain/alarm/alarm/enums/AlarmStatus.java
+++ backend/src/main/java/com/malnutrition/backend/domain/alarm/alarm/enums/AlarmStatus.java
@@ -1,6 +1,15 @@
 package com.malnutrition.backend.domain.alarm.alarm.enums;
 
 public enum AlarmStatus {
-    UNREAD,  // 읽지 않음
-    READ     // 읽음
+    UNREAD("읽지 않음"),
+    READ("읽음");
+    private final String description;
+
+    AlarmStatus(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
--- backend/src/main/java/com/malnutrition/backend/domain/alarm/alarmsetting/enums/AlarmSettingType.java
+++ backend/src/main/java/com/malnutrition/backend/domain/alarm/alarmsetting/enums/AlarmSettingType.java
@@ -2,7 +2,9 @@ package com.malnutrition.backend.domain.alarm.alarmsetting.enums;
 
 
 public enum AlarmSettingType {
-    COMMENT,
-    LIKE,
-    FOLLOW
+    NEW_LECTURE,         // 새 강의 업로드
+    LECTURE_STARTED,     // 강의 시작 알림
+    LECTURE_COMPLETED,   // 강의 완료 알림
+    NEW_COMMENT,         // 댓글 알림
+    SYSTEM_NOTIFICATION  // 시스템 공지
 }
