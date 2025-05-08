9d5611c31a3f288bfaea676dddc7ff40131ac2aa
gwon tae-yun <taeyun1118@naver.com>
2025-05-08 12:50:28 +0900

    feat: counseling 관련 엔티티 구현 추가 #2

--- backend/src/main/java/com/malnutrition/backend/domain/counseling/entity/Counseling.java
+++ backend/src/main/java/com/malnutrition/backend/domain/counseling/entity/Counseling.java
@@ -1,4 +1,29 @@
 package com.malnutrition.backend.domain.counseling.entity;
 
-public class Counseling {
+import com.malnutrition.backend.domain.user.entity.User;
+import com.malnutrition.backend.global.jpa.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+@Table(name = "counselings")
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Counseling extends BaseEntity {
+    private String title;
+
+    @Column(columnDefinition = "LONGTEXT")
+    private String content;
+
+    private String type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")  // FK 이름을 명시적으로 지정
+    private User user;
+
 }
