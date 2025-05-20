77c2bd9eb96d3678665e4daa494c8d6ba74701fb
thatisbummer <tjdals4358@naver.com>
2025-05-20 11:26:07 +0900

    refator/add permiturl

--- backend/src/main/java/com/malnutrition/backend/global/config/security/PermitUrl.java
+++ backend/src/main/java/com/malnutrition/backend/global/config/security/PermitUrl.java
@@ -9,19 +9,25 @@ public class PermitUrl {
             "/api/v1/exerciseSheets/**","/api/v1/admin/machine-types/**",
             "/api/v1/admin/machines/**","/api/v1/chatrooms/**",
             "/api/chat/**", "/ws-stomp/**", "/ws/**", "/chat/**",
-            "/api/v1/users/**","/api/v1/chats/**","/api/v1/machine-exercises/**"
+            "/api/v1/users/**","/api/v1/chats/**","/api/v1/machine-exercises/**",
+
     };
     public static final String[] GET_URLS = {
             "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
             "/actuator/**", "/error", "/css/**", "/js/**",
-            "/api/v1/images/view/*", "/api/v1/email/join/send"
+            "/api/v1/images/view/*", "/api/v1/email/join/send",
+            "/api/v1/lectures/**", "/api/v1/lecture_categories/**",
+            "/api/v1/lectureUsers/**", "/api/v1/curriculums/**",
+            "/api/v1/curriculum-progress/**"
     };
 
     public static final String[] POST_URLS = {
             "/api/v1/users/join", "/api/v1/users/login",
             "/api/v1/email/password/send", "/api/v1/email/verify",
             "/api/v1/email/join/send","/api/v1/sms/join/send-code", "/api/v1/sms/join/verify-code",
-            "/api/v1/sms/find-email/verify-code", "/api/v1/sms/find-email/send-code"
+            "/api/v1/sms/find-email/verify-code", "/api/v1/sms/find-email/send-code",
+            "/api/v1/lectures", "/api/v1/lecture_categories", "/api/v1/lectureUsers",
+            "/api/v1/curriculums", "/api/v1/curriculum-progress"
     };
 
     public static final String[] PUT_URLS = {
