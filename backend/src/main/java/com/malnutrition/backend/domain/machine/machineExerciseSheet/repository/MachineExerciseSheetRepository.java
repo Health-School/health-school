31a917316578e15837ccd543dd63d8bfef4d1d5d
gwon tae-yun <taeyun1118@naver.com>
2025-05-22 10:04:53 +0900

    fix: exerciseSheet eager to lazy #118

--- backend/src/main/java/com/malnutrition/backend/domain/machine/machineExerciseSheet/repository/MachineExerciseSheetRepository.java
+++ backend/src/main/java/com/malnutrition/backend/domain/machine/machineExerciseSheet/repository/MachineExerciseSheetRepository.java
@@ -3,10 +3,20 @@ package com.malnutrition.backend.domain.machine.machineExerciseSheet.repository;
 import com.malnutrition.backend.domain.machine.machineExerciseSheet.entity.MachineExerciseSheet;
 import com.malnutrition.backend.domain.user.exercisesheet.entity.ExerciseSheet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MachineExerciseSheetRepository extends JpaRepository<MachineExerciseSheet, Long> {
-    List<MachineExerciseSheet> findByMachineId(Long machineId);
-    List<MachineExerciseSheet> findByExerciseSheet(ExerciseSheet exerciseSheet);
+
+    @Query("SELECT mes FROM MachineExerciseSheet mes " +
+            "JOIN FETCH mes.machine m " +
+            "WHERE m.id = :machineId")
+    List<MachineExerciseSheet> findByMachineId(@Param("machineId") Long machineId);
+
+    @Query("SELECT mes FROM MachineExerciseSheet mes " +
+            "JOIN FETCH mes.exerciseSheet es " +
+            "WHERE es = :exerciseSheet")
+    List<MachineExerciseSheet> findByExerciseSheet(@Param("exerciseSheet") ExerciseSheet exerciseSheet);
 }
