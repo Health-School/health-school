69cf41bece753bc2cfeb2ff0d20c3d10f131f317
yoogwangryun <gry0244@naver.com>
2025-05-29 10:21:14 +0900

    fix/208/footer-수정

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -5,41 +5,22 @@ export default function Footer() {
   return (
     <footer className="bg-gray-800 w-full mt-auto">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-2 gap-8 md:grid-cols-4">
-          <div>
-            <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-              회사소개
-            </h3>
-            <ul className="mt-4 space-y-4">
-              <li>
-                <Link
-                  href="/about"
-                  className="text-base text-gray-300 hover:text-white"
-                >
-                  About
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-              고객센터
-            </h3>
-            <ul className="mt-4 space-y-4">
-              <li>
-                <Link
-                  href="/contact"
-                  className="text-base text-gray-300 hover:text-white"
-                >
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </div>
+        <div className="text-left text-gray-300 space-y-2">
+          <p>
+            고객의 소중한 의견을 바탕으로 더 나은 서비스를 제공하기 위해
+            노력합니다.
+            <br />
+            고객님의 소중한 의견을 언제든지 환영합니다.
+          </p>
+          <p className="mt-4">
+            <span className="font-semibold">팀장:</span> 강성민 ㅣ{" "}
+            <span className="font-semibold">부팀장:</span> 강준호 ㅣ{" "}
+            <span className="font-semibold">팀원 :</span> 권태윤, 최정인, 유광륜
+          </p>
         </div>
         <div className="mt-8 border-t border-gray-700 pt-8">
-          <p className="text-base text-gray-400 xl:text-center">
-            &copy; 2024 Health School. All rights reserved.
+          <p className="text-base text-gray-400 xl:text-left">
+            &copy; 2025 healthschool. All rights reserved.
           </p>
         </div>
       </div>
