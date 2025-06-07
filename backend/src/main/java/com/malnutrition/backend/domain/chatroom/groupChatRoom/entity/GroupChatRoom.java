db78135d9b8a98bae10016cbf9bebbab96d0ffd8
gwon tae-yun <taeyun1118@naver.com>
2025-06-07 20:15:56 +0900

    fix:그룹채팅방 엔티티 수정 #269

--- backend/src/main/java/com/malnutrition/backend/domain/chatroom/groupChatRoom/entity/GroupChatRoom.java
+++ backend/src/main/java/com/malnutrition/backend/domain/chatroom/groupChatRoom/entity/GroupChatRoom.java
@@ -2,6 +2,7 @@ package com.malnutrition.backend.domain.chatroom.groupChatRoom.entity;
 
 import com.malnutrition.backend.domain.chatroom.groupChatMessage.entity.GroupChatMessage;
 import com.malnutrition.backend.domain.chatroom.groupChatUser.entity.GroupChatUser;
+import com.malnutrition.backend.domain.user.user.entity.User;
 import com.malnutrition.backend.global.jpa.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -19,6 +20,10 @@ public class GroupChatRoom extends BaseEntity {
 
     @Column(nullable = false)
     private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trainer_id", nullable = false)
+    private User createdBy;
 
     @OneToMany(mappedBy = "groupChatRoom", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<GroupChatUser> participants;
